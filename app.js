2f163a0c08099fa2e1d9512124db0a03093eecd3
SaumyaSingh-03 <saumyasingh23480@gmail.com>
2026-01-22 15:24:09 +0530

    stringUtils_output

---
+++ app.js
@@ -0,0 +1,8 @@
+const stringUtils = require('./Q2_gitstringUtils');
+
+const myString = "hello bridgelabz";
+
+console.log("Original:", myString);
+console.log("Capitalized:", stringUtils.capitalize(myString));
+console.log("Reversed:", stringUtils.reverseString(myString));
+console.log("Vowel Count:", stringUtils.countVowels(myString));
